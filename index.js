ee177851165f173a8f9f7b6aa3608c259244c020
Daniel Andrews <danielandrews@Daniels-MacBook-Pro-2.local>
2016-12-19 19:58:12 -0800

    Added more parts of Resume format
    working on education / fixed .gitignore

--- index.js
+++ index.js
@@ -4,12 +4,36 @@ const PDFDocument = require('pdfkit');
 const fs = require('fs');
 const doc = new PDFDocument;
 
-let name      = 'Daniel Andrews'
-let address   = '2430 Piedmont Ave, Berkeley CA 94704'
-let email     = 'DanielAndrews@berkeley.edu'
-let phoneNum  = '(310) 424 8136'
+let name      = 'Daniel Andrews';
+let address   = '2430 Piedmont Ave, Berkeley CA 94704';
+let email     = 'DanielAndrews@berkeley.edu';
+let phone_num = '(310) 424 8136';
 
-let dot = ' • '
+let coll_name   = 'University of California, Berkeley';
+let coll_GPA    = '2.823';
+let coll_degree = 'Bachelor of Science: Electrical Engineering & Computer Science';
+let coll_date   = '2016 - 2020';
+let coll_loc    = 'Berkeley, CA'; 
+
+let coll_work = [
+    'Information & Designing Systems I',
+    'The Structure & Interpretation of Computer Programs',
+    'Multivariable Calculus'
+]; 
+let exp_coll_work = [
+    'Data Structures',
+    'Information & Designing Systems II',
+    'Discrete Mathematics & Probablility'
+];
+
+let HS_name       = 'Alexander Hamilton High School';
+let HS_date       = '2012 - 2016'
+let HS_GPA_weight = '4.813';
+let HS_GPA        = '4.000';
+let HS_school_loc = 'Los Angeles, CA';
+let HS_awards     = '19 AP Tests, Valedictorian, Hardest Working';
+
+let dot = ' • ';
 
 
 let docname = (new Date()).getTime() + '.pdf';
@@ -21,50 +45,65 @@ doc.pipe(fs.createWriteStream('./pdfs/' + docname));
 doc.fontSize(40);
 
 doc.text(name, {
-    align: 'center'
+    align: 'center',
+    lineGap: -20
 });
 
 //Address
 doc.fontSize(10);
-doc.lineGap(-20);
 doc.moveDown();
 
 doc.text(address, {
-    align: 'center'
+    align: 'center',
+    lineGap: -10
 })
 
 //Contact
-doc.lineGap(10)
 doc.moveDown()
 
-doc.text(email + dot + phoneNum, {
-    align: 'center'
+doc.text(email + dot + phone_num, {
+    align: 'center',
+    lineGap: -20
 });
 
 
 //Educatuin
 //Education Title
-doc.lineGap(-30)
 doc.fontSize(22);
 doc.moveDown();
 
 doc.text('Education', {
     align: 'left',
+    lineGap: -105
 });
 
 //Education Bar
 //******Should eventually change this to a vector line!!!*******
 doc.fontSize(50);
-doc.lineGap(-75);
 doc.moveDown();
 
-doc.text('________________');
+doc.text('________________', {
+    lineGap: -20
+});
 
-//Education list
+//Education Highschool
 doc.fontSize(12);
-doc.lineGap(0);
 doc.moveDown();
 
+doc.text(coll_name + ' - ' + coll_GPA, {
+    align: 'left',
+    lineGap: -10
+});
+
+doc.moveDown();
+doc.text('test', {
+    lineGap: -10
+});
+
+doc.moveDown();
+doc.text('test123', {
+    lineGap: -10
+});
 
 
 
