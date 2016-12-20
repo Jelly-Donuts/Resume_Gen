caded222054688c1b00313db2d49d8292e18cbd1
JeremyEphron <jeremybarenholtz@Jeremys-MacBook-Pro.local>
2016-12-20 00:20:03 -0800

    PDF Formatting in Index 2, added fonts for first template

--- index2.js
+++ index2.js
@@ -4,68 +4,452 @@ const PDFDocument = require('pdfkit');
 const fs = require('fs');
 const doc = new PDFDocument;
 
-let name      = 'Daniel Andrews'
-let address   = '2430 Piedmont Ave, Berkeley CA 94704'
-let email     = 'DanielAndrews@berkeley.edu'
-let phoneNum  = '(310) 424 8136'
-
+let name      = 'John Doe'
+let address   = '1234 Main Street, Anytown, STATE 56789'
+let phoneNum  = '(999) 999-9999'
+let email     = 'email@address.com'
 let dot = ' • '
-
 
 let docname = (new Date()).getTime() + '.pdf';
 doc.pipe(fs.createWriteStream('./pdfs/' + docname));
 
+//Template 1
+//Template 1:Fonts
+doc.registerFont('Heading Name', './fonts/Didot.ttf');
+doc.registerFont('Contact Info', './fonts/AvenirNext-UltraLight.ttf');
+doc.registerFont('Section Title', './fonts/Georgia Bold.ttf');
+doc.registerFont('Content Regular', './fonts/AvenirNext-Regular.ttf');
+doc.registerFont('Content Italics', './fonts/AvenirNext-Italic.ttf');
+doc.registerFont('Content Bold1', './fonts/AvenirNext-Medium.ttf');
+doc.registerFont('Content Bold2', './fonts/AvenirNext-DemiBold.ttf');
 
-//Top Info
-//Name
-doc.fontSize(40);
+//Template 1:Font Sizes
+let headingFontSize = 24;
+let titleFontSize = 12;
+let contentFontSize = 10.2;
+doc.lineGap(-0.2);
 
-doc.text(name, {
-    align: 'center'
-});
+//Header
+//Header:Name
+doc.moveUp(3.5)
+	.font('Heading Name')
+	.fontSize(headingFontSize)
+	.text(name, {
+    	align: 'center'
+	});
 
-//Address
-doc.fontSize(10);
-doc.lineGap(-20);
-doc.moveDown();
-
-doc.text(address, {
-    align: 'center'
-})
-
-//Contact
-doc.lineGap(10)
-doc.moveDown()
-
-doc.text(email + dot + phoneNum, {
-    align: 'center'
-});
-
-
-//Educatuin
-//Education Title
-doc.lineGap(-30)
-doc.fontSize(22);
-doc.moveDown();
-
-doc.text('Education', {
-    align: 'left',
-});
-
-//Education Bar
-//******Should eventually change this to a vector line!!!*******
-doc.fontSize(50);
-doc.lineGap(-75);
-doc.moveDown();
-
-doc.text('________________');
-
-//Education list
-doc.fontSize(12);
-doc.lineGap(0);
-doc.moveDown();
+//Header:Contact Info
+doc.font('Contact Info')
+	.fontSize(titleFontSize)
+	.text(address, {
+    	align: 'center'
+	})
+	.text(email + dot + phoneNum, {
+		align: 'center'
+	});
 
 
+//Education
+//Education:Title
+doc.font('Section Title')
+	.fontSize(titleFontSize)
+	.moveDown()
+	.text('EDUCATION', {
+    	align: 'left'
+	});
+
+//Education:Bar
+doc.lineWidth(1)
+   .moveTo(72, 114)
+   .lineTo(540, 114)
+   .stroke();
+
+//Education:University
+doc.font('Content Bold2')
+	.fontSize(contentFontSize)
+	.moveDown(0.2)
+	.text('University', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('City, STATE', {
+		align: 'right',
+	});
+
+//Education:University:Degree
+doc.font('Content Regular')
+	.text('Degree' + ' (Candidate) ' + 'in Major', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Italics')
+	.text('(Expected) ' + 'Month Year of Graduation', {
+		align: 'right'
+	});
+
+//Education:University:Certificate
+doc.font('Content Regular')
+	.text('Certificate (if you have one)', {
+		align: 'left'
+	});
+
+//Education:University:GPA
+doc.font('Content Regular')
+	.text('GPA ' + 'X.XX' + ' (include if >3)', {
+		align: 'left'
+	});
+
+//Education:University:Honors/Awards
+doc.font('Content Bold1')
+	.text('Honors/Awards: ', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('Honor' + ', Award' + ', Honor' + ', Award', {
+		align: 'left'
+	});
+
+//Education:University:Relevant Coursework
+doc.font('Content Bold1')
+	.text('Relevant Coursework: ', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('Course' + ', Work' + ', Course' + ', Work', {
+		align: 'left'
+	});
+
+//Education:High School
+doc.font('Content Bold2')
+	.fontSize(contentFontSize)
+	.moveDown(0.6)
+	.text('High School', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text(' – ', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Italics')
+	.text('GPA: XX.X (unweighted/weighted)', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('City, STATE', {
+		align: 'right',
+	});
+
+//Education:High School:Degree
+doc.font('Content Regular')
+	.text('High School Diploma/GED', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Italics')
+	.text('Month Year of Graduation', {
+		align: 'right'
+	});
+
+//Education:High School:Honors/Awards
+doc.font('Content Bold1')
+	.text('Honors/Awards: ', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('Honor' + ', Award' + ', Honor' + ', Award', {
+		align: 'left'
+	});
+
+//Professional Experience
+//Professional Experience:Title
+doc.font('Section Title')
+	.fontSize(titleFontSize)
+	.moveDown()
+	.text('PROFESSIONAL EXPERIENCE', {
+    	align: 'left'
+	});
+
+//Professional Experience:Bar
+doc.lineWidth(1)
+   .moveTo(72, 275)
+   .lineTo(540, 275)
+   .stroke();
+
+//Professional Experience:Employer 1
+doc.font('Content Bold2')
+	.fontSize(contentFontSize)
+	.moveDown(0.2)
+	.text('Employer', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('City, STATE', {
+		align: 'right',
+	});
+
+//Professional Experience:Employer 1:Title/Position
+doc.font('Content Italics')
+	.text('Title/Position', {
+		align: 'left',
+		continued: true
+	})
+	.text('Month Year Start' + '–' + 'Month Year End', {
+		align: 'right'
+	});
+
+//Professional Experience:Employer 1: Content
+doc.font('Content Regular')
+	.text(' ' + dot + ' ' + 'Action verb + Project + Result', {
+		align: 'left'
+	})
+	.text(' ' + dot + ' ' + 'Accomplishments Achieved', {
+		align: 'left'
+	})
+	.text(' ' + dot + ' ' + 'Quantify number of people/items/data you worked with', {
+		align: 'left'
+	});
+
+//Professional Experience:Employer 2
+doc.font('Content Bold2')
+	.fontSize(contentFontSize)
+	.moveDown(0.6)
+	.text('Employer', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('City, STATE', {
+		align: 'right',
+	});
+
+//Professional Experience:Employer 2:Title/Position
+doc.font('Content Italics')
+	.text('Title/Position', {
+		align: 'left',
+		continued: true
+	})
+	.text('Month Year Start' + '–' + 'Month Year End', {
+		align: 'right'
+	});
+
+//Professional Experience:Employer 2: Content
+doc.font('Content Regular')
+	.text(' ' + dot + ' ' + 'Action verb + Project + Result', {
+		align: 'left'
+	})
+	.text(' ' + dot + ' ' + 'Accomplishments Achieved', {
+		align: 'left'
+	})
+	.text(' ' + dot + ' ' + 'Quantify number of people/items/data you worked with', {
+		align: 'left'
+	});
+
+//Leadership and Service
+//Leadership and Service:Title
+doc.font('Section Title')
+	.fontSize(titleFontSize)
+	.moveDown()
+	.text('LEADERSHIP AND SERVICE', {
+    	align: 'left'
+	});
+
+//Leadership and Service:Bar
+doc.lineWidth(1)
+   .moveTo(72, 450)
+   .lineTo(540, 450)
+   .stroke();
+
+//Leadership and Service:Organization 1
+doc.font('Content Bold2')
+	.fontSize(contentFontSize)
+	.moveDown(0.2)
+	.text('Organization', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('City, STATE', {
+		align: 'right',
+	});
+
+//Leadership and Service:Organization 1:Title/Role
+doc.font('Content Italics')
+	.text('Title/Role', {
+		align: 'left',
+		continued: true
+	})
+	.text('Month Year Start' + '–' + 'Month Year End', {
+		align: 'right'
+	});
+
+//Leadership and Service:Organization 1: Content
+doc.font('Content Regular')
+	.text(' ' + dot + ' ' + 'Action verb + Project + Result', {
+		align: 'left'
+	})
+	.text(' ' + dot + ' ' + 'Accomplishments Achieved', {
+		align: 'left'
+	})
+	.text(' ' + dot + ' ' + 'Quantify number of people/items/data you worked with', {
+		align: 'left'
+	});
+
+//Leadership and Service:Organization 2
+doc.font('Content Bold2')
+	.fontSize(contentFontSize)
+	.moveDown(0.6)
+	.text('Organization', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('City, STATE', {
+		align: 'right',
+	});
+
+//Leadership and Service:Organization 2:Title/Role
+doc.font('Content Italics')
+	.text('Title/Role', {
+		align: 'left',
+		continued: true
+	})
+	.text('Month Year Start' + '–' + 'Month Year End', {
+		align: 'right'
+	});
+
+//Leadership and Service:Organization 2: Content
+doc.font('Content Regular')
+	.text(' ' + dot + ' ' + 'Action verb + Project + Result', {
+		align: 'left'
+	})
+	.text(' ' + dot + ' ' + 'Accomplishments Achieved', {
+		align: 'left'
+	})
+	.text(' ' + dot + ' ' + 'Quantify number of people/items/data you worked with', {
+		align: 'left'
+	});
+
+
+//Skills
+//Skills:Title
+doc.font('Section Title')
+	.fontSize(titleFontSize)
+	.moveDown()
+	.text('SKILLS', {
+    	align: 'left'
+	});
+
+//Skills:Bar
+doc.lineWidth(1)
+   .moveTo(72, 625)
+   .lineTo(540, 625)
+   .stroke();
+
+//Skills:Domain 1
+doc.font('Content Bold1')
+	.fontSize(contentFontSize)
+	.moveDown(0.2)
+	.text('Domain: ', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('Skill' + ', Skill' + ', Skill' + ', Skill', {
+		align: 'left'
+	});
+
+//Skills:Domain 2
+doc.font('Content Bold1')
+	.text('Domain: ', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('Skill' + ', Skill' + ', Skill' + ', Skill', {
+		align: 'left'
+	});
+
+//Skills:Domain 3
+doc.font('Content Bold1')
+	.text('Domain: ', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('Skill' + ', Skill' + ', Skill' + ', Skill', {
+		align: 'left'
+	});
+
+
+//Activities
+//Activities:Title
+doc.font('Section Title')
+	.fontSize(titleFontSize)
+	.moveDown()
+	.text('ACTIVITIES', {
+    	align: 'left'
+	});
+
+//Activities:Bar
+doc.lineWidth(1)
+   .moveTo(72, 696)
+   .lineTo(540, 696)
+   .stroke();
+
+//Activities:Activity 1
+doc.font('Content Italics')
+	.fontSize(contentFontSize)
+	.moveDown(0.2)
+	.text('Title/Role, ', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('Group/Organization', {
+		align: 'left',
+		continued: true
+	})
+	.text('Month Year Start' + '–' + 'Month Year End', {
+		align: 'right'
+	});
+
+//Activities:Activity 2
+doc.font('Content Italics')
+	.text('Title/Role, ', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('Group/Organization', {
+		align: 'left',
+		continued: true
+	})
+	.text('Month Year Start' + '–' + 'Month Year End', {
+		align: 'right'
+	});
+
+//Activities:Activity 3
+doc.font('Content Italics')
+	.text('Title/Role, ', {
+		align: 'left',
+		continued: true
+	})
+	.font('Content Regular')
+	.text('Group/Organization', {
+		align: 'left',
+		continued: true
+	})
+	.text('Month Year Start' + '–' + 'Month Year End', {
+		align: 'right'
+	});
 
 
 doc.end();
