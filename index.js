cc393334d365c92d27d08c80e120d330aa7a6acb
JeremyEphron <jeremyephron@gmail.com>
2016-12-30 03:42:49 -0800

    d

--- index.js
+++ index.js
@@ -18,16 +18,13 @@ app.set('views', __dirname + '/site/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-// router.post('/pdfgen', function(req, res) {
-//     console.log('here');
-//     var content = JSON.stringify(req.body, null, 2);
-//     console.log(content);
-//     console.log(pdfGen.handler(content));
-// });
+router.post('/pdfgen', function(req, res) {
+    console.log('here');
+    var content = JSON.stringify(req.body, null, 2);
+    console.log(content);
+    console.log(pdfGen.handler(content));
+});
 
-router.get("/pdfgen", function(req, res){
-	res.render('pages/demo');
-})
 
 // app.get('/', function(request, response) {
 //   response.render('pages/index')
