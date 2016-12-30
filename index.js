94f3ae84d44e8233ec3156f16bdb7bf3f880ca2f
JeremyEphron <jeremyephron@gmail.com>
2016-12-29 20:09:20 -0800

    Version 1.0 ready to launch

--- index.js
+++ index.js
@@ -7,29 +7,27 @@ docName = pdfGen.PDFGenerator()
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + 'site/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
+  response.render('index')
 });
 
 app.get('/pdfcount', function(request, response){
-  response.sendFile(path.join(__dirname, '../backend/count.txt'))
+  response.sendFile(path.join(__dirname, './backend/count.txt'))
 });
 
 app.get('/pdfgen', function(request, response){
-	res.sendFile(path.join(__dirname, '../working-pdf-opener/pdfs', docName));
+	res.sendFile(path.join(__dirname, './Resume_Gen/pdfs', docName));
 });
 
 app.get('/pdfgen', function(request, response){
   response.send(pdfGen.docNameGen());
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
