f3f6dac2af6887d4ae95a27b350c977ffc94d431
Daniel Andrews <danielandrews@berkeley.edu>
2016-12-30 04:53:05 -0800

    a

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
-var bodyParser = require('body-parser');
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
-
-app.use(express.json());       // to support JSON-encoded bodies
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use('/static', express.static(path.join(__dirname + '/public')));
