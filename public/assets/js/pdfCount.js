8828197437ae986ffce67ca51693ffc7e9f9a5df
JeremyEphron <jeremyephron@gmail.com>
2016-12-29 22:41:13 -0800

    demo

--- public/assets/js/pdfCount.js
+++ public/assets/js/pdfCount.js
@@ -1,20 +1,19 @@
-function () {
-    $.get('/pdfcount', function(data){;
-        document.getElementById('pdfcount').innerHTML = 1;
-        console.log(document.getElementById('pdfcount').innerHTML);
+
+$.get('/pdfcount', function(data){;
+    document.getElementById('pdfcount').innerHTML = 1;
+    console.log(document.getElementById('pdfcount').innerHTML);
+});
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
     });
-    $('.count').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 4000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
-};
+});
 
 
 
