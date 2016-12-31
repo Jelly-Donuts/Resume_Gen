6e03c1068fad4981b18775e2b8c4c917d657fde1
JeremyEphron <jeremyephron@gmail.com>
2016-12-31 04:03:19 -0800

    d

--- public/assets/js/add_remove_buttons.js
+++ public/assets/js/add_remove_buttons.js
@@ -1,104 +1,114 @@
+// Remove a field/section that has been added
+function delClone(item) {
+
+    // Confirm that they wish to delete field/section
+    if (confirm("Are you sure you wish to remove this field? This cannot be undone."))
+    {
+        /* Use clicked button's attributes to identify removal target and 
+           corresponding add button */
+        let targetID   = '#' + $(item).attr('for'),
+            targetAttr = $(targetID).attr('data-clone'),
+            btnAddPair = '#' + $(item).attr('data-btn-add'),
+
+            // How many cloned input fields we currently have
+            num        = $("[data-clone='" + targetAttr + "']").length - 1;
+
+            // Remove the field/section (after slideUp animation is complete)
+            $('#' + targetAttr + num).slideUp(function () {
+                $(this).remove();
+            });
+
+            // If only one element remains, disable the "remove" button
+            if (num === 1) {
+                $(item).attr('disabled', true);
+            }
+
+        // Enable the add button and change it's text to original value
+        let text = $(btnAddPair).attr('data-field');
+        $(btnAddPair).attr('disabled', false).val(text);
+    }
+    return false;
+}
+
 // Clone a hidden element to add a field/section
-function addClone(item){
+function addClone(item) {
 
     /* Use the clicked button's attributes to identify the object being cloned 
        and the corresponding delete button */
-    let targetID   = '#' + $(item).attr('for');
-    let targetAttr = $(targetID).attr('data-clone');
-    let btnDelPair = '#' + $(item).attr('data-btn-del');
+    let targetID   = '#' + $(item).attr('for'),
+        targetAttr = $(targetID).attr('data-clone'),
+        btnDelPair = '#' + $(item).attr('data-btn-del'),
 
         // How many cloned input fields we currently have
-    let num        = $("[data-clone='" + targetAttr + "']").length - 1;
-    let newNum     = num + 1;
-    let newID = targetAttr + newNum;
+        num        = $("[data-clone='" + targetAttr + "']").length - 1,
+        newNum     = num + 1,
+        newID      = targetAttr + newNum,
 
         // Create the new element via clone(), and change it's id
-    let newElem = $(targetID).clone().attr('id', newID).fadeIn();
+        newElem = $(targetID).clone().attr('id', newID).fadeIn();
 
     newElem.removeClass("hidden");
 
-    // Heading
+    // Change the heading text
     newElem.find('.' + targetAttr + '_heading').text(targetAttr + ' #' + newNum);
 
     // Insert the new element after the last cloned input field
     $('#' + targetAttr + num).after(newElem);
     newElem.find('input:first').focus();
 
-    // Add/Remove Content buttons
+    // Activate any add and remove buttons cloned
     for (let i = 0; i < newElem.find('.btnAdd').length; i++) {
 
-        let btnAddID = targetID + 'btnAdd' + i,
+        /* From the attributes of the add button, identify the clone target and
+           the corresponding delete button */
+        let btnAddID    = targetID + 'btnAdd' + i,
             newBtnAddID = newID + 'btnAdd' + i,
-            btnDelID = '#' + newElem.find(btnAddID).attr('data-btn-del'),
+            btnDelID    = '#' + newElem.find(btnAddID).attr('data-btn-del'),
             newBtnDelID = newID + 'btnDel' + i,
-            inputID = '#' + newElem.find(btnAddID).attr('for'),
-            inputType = newElem.find(inputID).attr('data-field'),
-            newInputID = newID + inputType + 0;
+            inputID     = '#' + newElem.find(btnAddID).attr('for'),
+            inputType   = newElem.find(inputID).attr('data-field'),
+            newInputID  = newID + inputType + 0;
 
+        // Change the attributes of the clone target
         newElem.find(inputID).attr('data-clone', newID + inputType);
         newElem.find(inputID).attr('id', newInputID);
 
-        newElem.find(btnDelID).attr('for', newInputID);
+        // Change the attributes of the delete button
+        newElem.find(btnDelID).attr('for'         , newInputID);
         newElem.find(btnDelID).attr('data-btn-add', newBtnAddID);
-        newElem.find(btnDelID).attr('disabled', true);
-        newElem.find(btnDelID).attr('id', newBtnDelID);
+        newElem.find(btnDelID).attr('disabled'    , true);
+        newElem.find(btnDelID).attr('id'          , newBtnDelID);
 
-        newElem.find(btnAddID).attr('for', newInputID);
+        // Change the attributes of the add button
+        newElem.find(btnAddID).attr('for'         , newInputID);
         newElem.find(btnAddID).attr('data-btn-del', newBtnDelID);
-        newElem.find(btnAddID).attr('disabled', false);
-        newElem.find(btnAddID).attr('id', newBtnAddID);
+        newElem.find(btnAddID).attr('disabled'    , false);
+        newElem.find(btnAddID).attr('id'          , newBtnAddID);
 
         // Bind addClone function to add button
-        $('#' + newBtnAddID).on("click", function(){addClone(this)});
+        $('#' + newBtnAddID).on('click', function(){addClone(this);});
 
         // Bind delClone function to delete button
-        $('#' + newBtnDelID).on("click", function(){delClone(this)});
+        $('#' + newBtnDelID).on('click', function(){delClone(this);});
 
-    };
+    }
 
-    // Enable the delete button
+    // Enable the delete button after field/section has been added
     $(btnDelPair).attr('disabled', false);
 
-    // Limit maximum amount of cloned fields
-    if (newNum === 5){
+    // Limit maximum amount of added fields
+    if (newNum === 5) {
         $(item).attr('disabled', true).val("Max Reached");
     }
-}
-
-function delClone(item) {
-
-    // Confirmation dialog box
-    if (confirm("Are you sure you wish to remove this field? This cannot be undone.")){
-
-    let targetID   = '#' + $(item).attr('for'),
-        targetAttr = $(targetID).attr('data-clone'),
-        btnAddPair = '#' + $(item).attr('data-btn-add')
-
-        // How many cloned input fields we currently have
-        num        = $("[data-clone='" + targetAttr + "']").length - 1,
-        $('#' + targetAttr + num).slideUp(function () {
-            $(this).remove();
-        });
-
-        // If only one element remains, disable the "remove" button
-        if (num === 1){
-            $(item).attr('disabled', true);
-        }
-
-    // Enable the "add" button.
-    let text = $(btnAddPair).attr('data-field');
-    $(btnAddPair).attr('disabled', false).val(text);
-    }
-    return false;
 }
 
 $(document).ready(function () {
 
     // Bind addClone() to all add buttons
-    $('.btnAdd').click(function() {addClone(this)});
+    $('.btnAdd').click(function() {addClone(this);});
 
     // Bind delClone() to all delete buttons
-    $('.btnDel').click(function() {delClone(this)});
+    $('.btnDel').click(function() {delClone(this);});
 
     // Enable add buttons
     $('.btnAdd').attr('disabled', false);
@@ -106,45 +116,6 @@ $(document).ready(function () {
     // Disable delete buttons
     $('.btnDel').attr('disabled', true);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- public/assets/js/formSubmit.js
+++ public/assets/js/formSubmit.js
@@ -1,132 +1,198 @@
 $(function (){
 	$("form").submit(function( ) {
 
-		// Cloned variables
-		var uniAwardStr = '';
-		for (let i = 1; i < $("[data-clone='award']").length + 1; i++){
-			if ($("#award" + i).find('.awardInput').val() !== ''){
-				if (i > 1){
-					uniAwardStr += ', ';
-				}
-				uniAwardStr += $("#award" + i).find('.awardInput').val();
-			}
-		}
-
-		var uniCourseStr = '';
-		for (let i = 1; i < $("[data-clone='course']").length + 1; i++){
-			if ($("#course" + i).find('.courseInput').val() !== ''){
-				if (i > 1){
-					uniCourseStr += ', ';
-				}
-				uniCourseStr += $("#course" + i).find('.courseInput').val();
-			}
-		}
-
-		var hsAwardStr = '';
-		for (let i = 1; i < $("[data-clone='hsaward']").length + 1; i++){
-			if ($("#hsaward" + i).find('.hsawardInput').val() !== ''){
-				if (i > 1){
-					hsAwardStr += ', ';
-				}
-				hsAwardStr += $("#hsaward" + i).find('.hsawardInput').val();
-			}
-		}
-
-		const monthDict = {
-			'01' : 'January',
-			'02' : 'Febuary',
-			'03' : 'March',
-			'04' : 'April',
-			'05' : 'May',
-			'06' : 'June',
-			'07' : 'July',
-			'08' : 'August',
-			'09' : 'September',
-			'10' : 'October',
-			'11' : 'November',
-			'12' : 'December',
-		};
-
-		const dateChanger = function(date){
-			console.log('date', date);
-			const dateYear = date.substring(0, 4) || '';
-			let dateMon    = date.substring(5,7) || '';
-			dateMon        = monthDict[dateMon] || '';
-			return (dateMon + ' ' + dateYear);
-		};
-
-		const degreeDate = dateChanger($('#degreeMonth').val()) || '';
-		const hsDate = dateChanger($('#highschoolMonth').val()) || '';
-
-		// Form Info
 		let segmentContact = {
-	        name   : $('#firstName').val() + " "  + $('#lastName').val(),
-	        address: ($('#address').val() || '') + ", " + ($('#addressCity').val() || '') + ", " + ($('#addressState').val() || '') + " " + ($('#zipcode').val() || ''),
+	        name   : $('#contact').find('.firstname').val() + " "  + $('#contact').find('.lastname').val(),
+	        address: ($('#contact').find('.address').val() || '') + ", " + ($('#contact').find('.city').val() || '') + ", " + ($('#contact')find('state').val() || '') + " " + ($('#contact')find('zipcode').val() || ''),
 	        reach  : [
-	            $('#email').val() || '',
-	            $('#phone').val() || '',
+	            $('#contact')find('email').val() || '',
+	            $('#contact')find('phone').val() || '',
 	            ''
 	        ]
-		};
-
+		}
 		let segmentEducation = {
-	        title: 'EDUCATION',
-	        items: [
-	            {
-	                city      : $('#universityCity').val(), 
-	                state     : $('#universityState').val(),
-	                start_date: degreeDate,
-	                end_date  : '',
-	                lines: [
-	                    {
-	                        bullet : false,
-	                        title  : $('#university').val(),
-	                        content: ''
-	                    },
-	                    {
-	                        bullet : false,
-	                        content: $('#degree').val() +  ' (Candidate) in Major'
-	                    },
-	                    {
-	                        bullet : false,
-	                        content: 'GPA ' + $('#universityGpa').val()
-	                    },
-	                    {
-	                        bullet : false,
-	                        title  : 'Honors/Awards: ',
-	                        content: uniAwardStr
-	                    },
-	                    {
-	                        bullet : false,
-	                        title  : 'Relevant Coursework: ',
-	                        content: uniCourseStr
-	                    }
-	                ]
-	            },
-	            {
-	                city      : $('#highschoolCity').val(),
-	                state     : $('#highschoolState').val(),
-	                start_date: hsDate,
-	                end_date  : "",
-	                lines: [
-	                    {
-	                        title  : $('#highschool').val(),
-	                        bullet : false,
-	                        content: ''
-	                    },
-	                    {
-	                        bullet : false,
-	                        content: 'High School Diploma - ' + $('#highschoolGpa').val() + 'GPA'
-	                    },
-	                    {
-	                        bullet : false,
-	                        title  : 'Honors/Awards: ',
-	                        content: hsAwardStr
-	                    }
-	                ]
-	            }
-	        ]
-	    };
+	        title: "",
+	        items: []
+	    }
+	    let segmentProfExp = {
+	        title: "",
+	        items: []
+	    }
+	    let segmentExtracur = {
+	        title: "",
+	        items: []
+	    }
+	    let segmentSkills = {
+	        title: "",
+	        items: []
+	    }
+
+		// //create the employer part of schema
+	 //    for(let i = 1; i < $('.employer').length; i++){
+	 //    	if (i === 1) {
+	 //    		segmentProfExp.title = 'PROFESSIONAL EXPERIENCE';
+	 //    	}
+	 //    	let employer = '#employer' + String(i);
+	 //    	let start    = dateChanger($(employer).find('.employerMonthStart').val());
+	 //    	let end      = dateChanger($(employer).find('.employerMonthEnd').val());
+
+	 //    	let employee = {
+  //               city      : $(employer).find('.employerCity').val(),
+  //               state     : $(employer).find('.employerState').val(),
+  //               start_date: start,
+  //               end_date  : end,
+  //               lines: [
+  //                   {
+  //                       title  : $(employer).find('.employerInput').val(),
+  //                       content: '',
+  //                   },
+  //                   {
+  //                       italics: true,
+  //                       content: $(employer).find('.employerTitle').val()
+  //                   }
+  //               ]
+	 //    	};
+
+	   		//Get job desription items
+	    	for (let j = 1; j < 6; j++) {
+	    		if ($(desc).find("[name='jobcontent']").val()) {
+		    		let desc = '#cloneId' + String.fromCharCode(97 + i) + String(j);
+		    		employee.lines.push({bullet: true, content: $(desc).find("[name='jobcontent']").val()});
+		    	}
+
+	    	}
+	    	segmentProfExp.items.push(employee);
+	    }
+
+
+
+
+		// // Cloned variables
+		// var uniAwardStr = '';
+		// for (let i = 1; i < $("[data-clone='award']").length + 1; i++){
+		// 	if ($("#award" + i).find('.awardInput').val() !== ''){
+		// 		if (i > 1){
+		// 			uniAwardStr += ', ';
+		// 		}
+		// 		uniAwardStr += $("#award" + i).find('.awardInput').val();
+		// 	}
+		// }
+
+		// var uniCourseStr = '';
+		// for (let i = 1; i < $("[data-clone='course']").length + 1; i++){
+		// 	if ($("#course" + i).find('.courseInput').val() !== ''){
+		// 		if (i > 1){
+		// 			uniCourseStr += ', ';
+		// 		}
+		// 		uniCourseStr += $("#course" + i).find('.courseInput').val();
+		// 	}
+		// }
+
+		// var hsAwardStr = '';
+		// for (let i = 1; i < $("[data-clone='hsaward']").length + 1; i++){
+		// 	if ($("#hsaward" + i).find('.hsawardInput').val() !== ''){
+		// 		if (i > 1){
+		// 			hsAwardStr += ', ';
+		// 		}
+		// 		hsAwardStr += $("#hsaward" + i).find('.hsawardInput').val();
+		// 	}
+		// }
+
+		// const monthDict = {
+		// 	'01' : 'January',
+		// 	'02' : 'Febuary',
+		// 	'03' : 'March',
+		// 	'04' : 'April',
+		// 	'05' : 'May',
+		// 	'06' : 'June',
+		// 	'07' : 'July',
+		// 	'08' : 'August',
+		// 	'09' : 'September',
+		// 	'10' : 'October',
+		// 	'11' : 'November',
+		// 	'12' : 'December',
+		// };
+
+		// const dateChanger = function(date){
+		// 	console.log('date', date);
+		// 	const dateYear = date.substring(0, 4) || '';
+		// 	let dateMon    = date.substring(5,7) || '';
+		// 	dateMon        = monthDict[dateMon] || '';
+		// 	return (dateMon + ' ' + dateYear);
+		// };
+
+		// const degreeDate = dateChanger($('#degreeMonth').val()) || '';
+		// const hsDate = dateChanger($('#highschoolMonth').val()) || '';
+
+		// Form Info
+		// let segmentContact = {
+	 //        name   : $('#firstName').val() + " "  + $('#lastName').val(),
+	 //        address: ($('#address').val() || '') + ", " + ($('#addressCity').val() || '') + ", " + ($('#addressState').val() || '') + " " + ($('#zipcode').val() || ''),
+	 //        reach  : [
+	 //            $('#email').val() || '',
+	 //            $('#phone').val() || '',
+	 //            ''
+	 //        ]
+		// };
+
+		// let segmentEducation = {
+	 //        title: 'EDUCATION',
+	 //        items: [
+	 //            {
+	 //                city      : $('#universityCity').val(), 
+	 //                state     : $('#universityState').val(),
+	 //                start_date: degreeDate,
+	 //                end_date  : '',
+	 //                lines: [
+	 //                    {
+	 //                        bullet : false,
+	 //                        title  : $('#university').val(),
+	 //                        content: ''
+	 //                    },
+	 //                    {
+	 //                        bullet : false,
+	 //                        content: $('#degree').val() +  ' (Candidate) in Major'
+	 //                    },
+	 //                    {
+	 //                        bullet : false,
+	 //                        content: 'GPA ' + $('#universityGpa').val()
+	 //                    },
+	 //                    {
+	 //                        bullet : false,
+	 //                        title  : 'Honors/Awards: ',
+	 //                        content: uniAwardStr
+	 //                    },
+	 //                    {
+	 //                        bullet : false,
+	 //                        title  : 'Relevant Coursework: ',
+	 //                        content: uniCourseStr
+	 //                    }
+	 //                ]
+	 //            },
+	 //            {
+	 //                city      : $('#highschoolCity').val(),
+	 //                state     : $('#highschoolState').val(),
+	 //                start_date: hsDate,
+	 //                end_date  : "",
+	 //                lines: [
+	 //                    {
+	 //                        title  : $('#highschool').val(),
+	 //                        bullet : false,
+	 //                        content: ''
+	 //                    },
+	 //                    {
+	 //                        bullet : false,
+	 //                        content: 'High School Diploma - ' + $('#highschoolGpa').val() + 'GPA'
+	 //                    },
+	 //                    {
+	 //                        bullet : false,
+	 //                        title  : 'Honors/Awards: ',
+	 //                        content: hsAwardStr
+	 //                    }
+	 //                ]
+	 //            }
+	 //        ]
+	 //    };
 	    
 		let segmentProfExp = {
 			title:"",
@@ -250,4 +316,6 @@ $(function (){
         });
     });
 });
+
+
 
