0b9101497ef639feaad58310d7c37f2fdf42ad73
JeremyEphron <jeremyephron@gmail.com>
2016-12-31 11:39:28 -0800

    d

--- public/assets/js/add_remove_buttons.js
+++ public/assets/js/add_remove_buttons.js
@@ -119,3 +119,5 @@ $(document).ready(function () {
 
 
 
+
+
--- public/assets/js/formSubmit.js
+++ public/assets/js/formSubmit.js
@@ -6,66 +6,212 @@ $(function (){
 	        address: ($('#contact').find('.address').val() || '') + ", " + ($('#contact').find('.city').val() || '') + ", " + ($('#contact').find('state').val() || '') + " " + ($('#contact').find('zipcode').val() || ''),
 	        reach  : [
 	            $('#contact').find('email').val() || '',
-	            $('#contact').find('phone').val() || '',
-	            ''
+	            $('#contact').find('phone').val() || ''
 	        ]
-		}
+		};
 		let segmentEducation = {
-	        title: "",
+	        title: '',
 	        items: []
-	    }
+	    };
 	    let segmentProfExp = {
-	        title: "",
+	        title: '',
 	        items: []
-	    }
+	    };
 	    let segmentExtracur = {
-	        title: "",
+	        title: '',
 	        items: []
-	    }
+	    };
 	    let segmentSkills = {
-	        title: "",
+	        title: '',
 	        items: []
+	    };
+
+		// Create the University part of schema
+	    for(let i = 1; i < $("[data-clone='university']").length; i++){
+	    	if (i === 1) {
+	    		segmentEducation.title = 'EDUCATION';
+	    	}
+	    	let university = '#univerisity' + i;
+	    	let universityObj = {
+                city      : $(university).find('.city').val(),
+                state     : $(university).find('.state').val(),
+                start_date: $(university).find('.month').val() + " " + $(university).find('.year').val(),
+                end_date  : '',
+                lines: [
+                    {
+                        title  : $(university).find('.name').val(),
+                    },
+                    {
+                        content: $(university).find('.degree').val() + " in " + $(university).find('.major').val()
+                    },
+                    {
+                        content: "GPA " + $(university).find('.gpa').val()
+                    },
+                    {
+                    	title: '',
+                    	content: '',
+                    },
+                    {
+                    	title: '',
+                    	content: '',
+                    },
+                ]
+	    	};
+
+	   		// Honors and Awards
+	    	for (let j = 1; j < $(university).find('.award').length ; j++) {
+	    		universityObj.lines[3].title = "Honors/Awards: ";
+	    		let awardID = university + 'award' + j;
+	    		if ($(university).find(awardID).val()) {
+	    			if (j !== 1) {
+	    				universityObj.lines[3].content += ', ';
+	    			}
+	    			universityObj.lines[3].content += $(awardID).val();
+		    	}
+	    	}
+	    	for (let k = 1; k < $(university).find('.course').length; k++) {
+	    		universityObj.lines[4].title = "Relevant Coursework: ";
+	    		let courseID = university + 'course' + k;
+	    		if ($(university).find(courseID).val()) {
+	    			if (k !== 1) {
+	    				universityObj.lines[4].content += ', ';
+	    			}
+	    			universityObj.lines[4].content += $(courseID).val();
+		    	}
+	    	}
+
+	    	segmentEducation.items.push(universityObj);
 	    }
 
-		// //create the employer part of schema
-	 //    for(let i = 1; i < $('.employer').length; i++){
-	 //    	if (i === 1) {
-	 //    		segmentProfExp.title = 'PROFESSIONAL EXPERIENCE';
-	 //    	}
-	 //    	let employer = '#employer' + String(i);
-	 //    	let start    = dateChanger($(employer).find('.employerMonthStart').val());
-	 //    	let end      = dateChanger($(employer).find('.employerMonthEnd').val());
+		// Create the High School part of schema
+	    for(let i = 1; i < $("[data-clone='highschool']").length; i++){
+	    	if ((i === 1) && (segmentEducation.title === '')) {
+	    		segmentEducation.title = 'EDUCATION';
+	    	}
+	    	let highschool = '#highschool' + i;
+	    	let highschoolObj = {
+                city      : $(highschool).find('.city').val(),
+                state     : $(highschool).find('.state').val(),
+                start_date: $(highschool).find('.month').val() + " " + $(highschool).find('.year').val(),
+                end_date  : '',
+                lines: [
+                    {
+                        title  : $(highschool).find('.name').val(),
+                    },
+                    {
+	                    content: 'High School Diploma - GPA ' + $(highschool).find('.gpa').val()
+	                },
+                    {
+                    	title: '',
+                    	content: '',
+                    }
+                ]
+	    	};
 
-	 //    	let employee = {
-  //               city      : $(employer).find('.employerCity').val(),
-  //               state     : $(employer).find('.employerState').val(),
-  //               start_date: start,
-  //               end_date  : end,
-  //               lines: [
-  //                   {
-  //                       title  : $(employer).find('.employerInput').val(),
-  //                       content: '',
-  //                   },
-  //                   {
-  //                       italics: true,
-  //                       content: $(employer).find('.employerTitle').val()
-  //                   }
-  //               ]
-	 //    	};
-
-	   	// 	//Get job desription items
-	    // 	for (let j = 1; j < 6; j++) {
-	    // 		if ($(desc).find("[name='jobcontent']").val()) {
-		   //  		let desc = '#cloneId' + String.fromCharCode(97 + i) + String(j);
-		   //  		employee.lines.push({bullet: true, content: $(desc).find("[name='jobcontent']").val()});
-		   //  	}
-
-	    // 	}
-	    // 	segmentProfExp.items.push(employee);
-	    // }
+	   		// Honors and Awards
+	    	for (let j = 1; j < $(highschool).find('.award').length ; j++) {
+	    		highschoolObj.lines[3].title = "Honors/Awards: ";
+	    		let awardID = highschool + 'award' + j;
+	    		if ($(highschool).find(awardID).val()) {
+	    			if (j !== 1) {
+	    				highschoolObj.lines[3].content += ', ';
+	    			}
+	    			highschoolObj.lines[3].content += $(awardID).val();
+		    	}
+	    	}
+	    	segmentEducation.items.push(highschoolObj);
+	    }
 
 
+		// Build the Professional Experience part of schema
+	    for(let i = 1; i < $("[data-clone='employer']").length; i++){
+	    	if (i === 1) {
+	    		segmentProfExp.title = 'PROFESSIONAL EXPERIENCE';
+	    	}
+	    	let employer = '#employer' + i;
+	    	let employerObj = {
+                city      : $(employer).find('.city').val(),
+                state     : $(employer).find('.state').val(),
+                start_date: $(employer).find('.monthStart').val() + " " + $(employer).find('.yearStart').val(),
+                end_date  : $(employer).find('.monthEnd').val() + " " + $(employer).find('.yearEnd').val(),
+                lines: [
+                    {
+                        title  : $(employer).find('.name').val(),
+                    },
+                    {
+                        italics: true,
+                        content: $(employer).find('.position').val(),
+                    },
+                ]
+	    	};
 
+	   		// Job description
+	    	for (let j = 0; j < $(employer).find('.content').length ; j++) {
+	    		let contentID = employer + 'description' + j;
+	    		if ($(employer).find(contentID).val()) {
+	    			employerObj.lines.push({bullet: true, content: $(contentID).val()});
+		    	}
+	    	}
+	    	segmentProfExp.items.push(employerObj);
+	    }
+
+
+        // Build the Leadership and Extracurricular part of schema
+        for(let i = 1; i < $("[data-clone='activity']").length; i++){
+            if (i === 1) {
+                segmentExtracur.title = 'PROFESSIONAL EXPERIENCE';
+            }
+            let activity = '#activity' + i;
+            let activityObj = {
+                city      : $(activity).find('.city').val(),
+                state     : $(activity).find('.state').val(),
+                start_date: $(activity).find('.monthStart').val() + " " + $(activity).find('.yearStart').val(),
+                end_date  : $(activity).find('.monthEnd').val() + " " + $(activity).find('.yearEnd').val(),
+                lines: [
+                    {
+                        title  : $(activity).find('.name').val(),
+                    },
+                    {
+                        italics: true,
+                        content: $(activity).find('.position').val(),
+                    },
+                ]
+            };
+
+            // Activity description
+            for (let j = 0; j < $(activity).find('.content').length ; j++) {
+                let contentID = activity + 'description' + j;
+                if ($(activity).find(contentID).val()) {
+                    activityObj.lines.push({bullet: true, content: $(contentID).val()});
+                }
+            }
+            segmentExtracur.items.push(activityObj);
+        }
+
+
+        // Build the Skills part of schema
+        for(let i = 1; i < $("[data-clone='skill']").length; i++){
+            if (i === 1) {
+                segmentSkills.title = 'SKILLS';
+                segmentSkills.items.push({lines: []});
+            }
+            let skill = '#skill' + i;
+            let skillObj = {
+                title  : $(skill).find('.category').val(),
+                content: $(skill).find('.content').val(),
+            };
+            segmentSkills.items.lines.push(skillObj);
+        }
+
+        const schema = {
+            contact : segmentContact,
+            segments: [
+                segmentEducation,
+                segmentProfExp,
+                segmentExtracur,
+                segmentSkills
+            ]
+        };
 
 		// // Cloned variables
 		// var uniAwardStr = '';
@@ -194,114 +340,106 @@ $(function (){
 	 //        ]
 	 //    };
 	    
-		let segmentProfExp = {
-			title:"",
-			items: [],
-		};
+		// let segmentProfExp = {
+		// 	title:"",
+		// 	items: [],
+		// };
 
-		//create the employer part of schema
-	    for(let i = 1; i < $('.employer').length; i++){
-	    	if (i === 1) {
-	    		segmentProfExp.title = 'PROFESSIONAL EXPERIENCE';
-	    	}
-	    	let employer = '#employer' + String(i);
-	    	let start    = dateChanger($(employer).find('.employerMonthStart').val());
-	    	let end      = dateChanger($(employer).find('.employerMonthEnd').val());
+		// //create the employer part of schema
+	 //    for(let i = 1; i < $('.employer').length; i++){
+	 //    	if (i === 1) {
+	 //    		segmentProfExp.title = 'PROFESSIONAL EXPERIENCE';
+	 //    	}
+	 //    	let employer = '#employer' + String(i);
+	 //    	let start    = dateChanger($(employer).find('.employerMonthStart').val());
+	 //    	let end      = dateChanger($(employer).find('.employerMonthEnd').val());
 
-	    	let employee = {
-                city      : $(employer).find('.employerCity').val(),
-                state     : $(employer).find('.employerState').val(),
-                start_date: start,
-                end_date  : end,
-                lines: [
-                    {
-                        title  : $(employer).find('.employerInput').val(),
-                        content: '',
-                    },
-                    {
-                        italics: true,
-                        content: $(employer).find('.employerTitle').val()
-                    }
-                ]
-	    	};
+	 //    	let employee = {
+  //               city      : $(employer).find('.employerCity').val(),
+  //               state     : $(employer).find('.employerState').val(),
+  //               start_date: start,
+  //               end_date  : end,
+  //               lines: [
+  //                   {
+  //                       title  : $(employer).find('.employerInput').val(),
+  //                       content: '',
+  //                   },
+  //                   {
+  //                       italics: true,
+  //                       content: $(employer).find('.employerTitle').val()
+  //                   }
+  //               ]
+	 //    	};
 
-	   		//Get job desription items
-	    	for (let j = 1; j < 6; j++) {
-	    		if ($(desc).find("[name='jobcontent']").val()) {
-		    		let desc = '#cloneId' + String.fromCharCode(97 + i) + String(j);
-		    		employee.lines.push({bullet: true, content: $(desc).find("[name='jobcontent']").val()});
-		    	}
+	 //   		//Get job desription items
+	 //    	for (let j = 1; j < 6; j++) {
+	 //    		if ($(desc).find("[name='jobcontent']").val()) {
+		//     		let desc = '#cloneId' + String.fromCharCode(97 + i) + String(j);
+		//     		employee.lines.push({bullet: true, content: $(desc).find("[name='jobcontent']").val()});
+		//     	}
 
-	    	}
-	    	segmentProfExp.items.push(employee);
-	    }
+	 //    	}
+	 //    	segmentProfExp.items.push(employee);
+	 //    }
 
-	    const schema = {
-			contact : segmentContact,
-			segments: [
-				segmentEducation,
-				segmentProfExp,
-			]
-		};
+		// //create the Leadership and Extracurricular part of schema
+	 //    for(let i = 1; i < $('.leadAndExtra').length; i++){
+	 //    	let activity = '#activity' + String(i);
+	 //    	let start    = dateChanger($(activity).find("[name='activityDateStart']").val());
+	 //    	let end      = dateChanger($(activity).find("[name='activityDateEnd']").val());
 
-		//create the Leadership and Extracurricular part of schema
-	    for(let i = 1; i < $('.leadAndExtra').length; i++){
-	    	let activity = '#activity' + String(i);
-	    	let start    = dateChanger($(activity).find("[name='activityDateStart']").val());
-	    	let end      = dateChanger($(activity).find("[name='activityDateEnd']").val());
-
-	    	let activityObj = {
-	    		title: 'LEADERSHIP AND EXTRACURRICULAR',
-				items: [
-	                {
-	                    city      : $(activity).find("[name='City']").val(),
-	                    state     : $(activity).find("[name='State']").val(),
-	                    start_date: start,
-	                    end_date  : end,
-	                    lines: [
-	                        {
-	                            title  : $(activity).find("[name='Organization']").val(),
-	                            content: '',
-	                            bullet : false
-	                        },
-	                        {
-	                            bullet : false,
-	                            italics: true,
-	                            content: $(activity).find("[name='position']").val()
-	                        }
-	                    ]
-	                }
-	            ]
-	    	};
-	    	//Get activity desription items
-	    	for (let j = 1; j < 6; j++) {
-	    		let desc = '#clone2Id' + String.fromCharCode(97 + i);
-	    		activityObj.items[0].lines.push({bullet: true, content: $(desc).find("[name='activityContent']").val()});
-	    	}
-	    	schema.segments.push(activityObj);
-	    }
+	 //    	let activityObj = {
+	 //    		title: 'LEADERSHIP AND EXTRACURRICULAR',
+		// 		items: [
+	 //                {
+	 //                    city      : $(activity).find("[name='City']").val(),
+	 //                    state     : $(activity).find("[name='State']").val(),
+	 //                    start_date: start,
+	 //                    end_date  : end,
+	 //                    lines: [
+	 //                        {
+	 //                            title  : $(activity).find("[name='Organization']").val(),
+	 //                            content: '',
+	 //                            bullet : false
+	 //                        },
+	 //                        {
+	 //                            bullet : false,
+	 //                            italics: true,
+	 //                            content: $(activity).find("[name='position']").val()
+	 //                        }
+	 //                    ]
+	 //                }
+	 //            ]
+	 //    	};
+	 //    	//Get activity desription items
+	 //    	for (let j = 1; j < 6; j++) {
+	 //    		let desc = '#clone2Id' + String.fromCharCode(97 + i);
+	 //    		activityObj.items[0].lines.push({bullet: true, content: $(desc).find("[name='activityContent']").val()});
+	 //    	}
+	 //    	schema.segments.push(activityObj);
+	 //    }
 
 
-		//create the Skills part of schema
-	    for(let i = 1; i < $('.skillClass').length; i++){
-	    	let skill = '#skill' + String(i);
+		// //create the Skills part of schema
+	 //    for(let i = 1; i < $('.skillClass').length; i++){
+	 //    	let skill = '#skill' + String(i);
 
-	    	let skillObj = {
-	    		title: 'SKILLS',
-				items: [
-	                {
-	                    lines: [
-	                        {
-	                            bullet : false,
-	                            title  : $(skill).find("[name='skillCat']").val(),
-	                            content: $(skill).find("[name='skillContent']").val()
-	                        },
-	                    ]
-	                }
-	            ]
-	    	};
-	    	schema.segments.push(skillObj);
-	    }
+	 //    	let skillObj = {
+	 //    		title: 'SKILLS',
+		// 		items: [
+	 //                {
+	 //                    lines: [
+	 //                        {
+	 //                            bullet : false,
+	 //                            title  : $(skill).find("[name='skillCat']").val(),
+	 //                            content: $(skill).find("[name='skillContent']").val()
+	 //                        },
+	 //                    ]
+	 //                }
+	 //            ]
+	 //    	};
+	 //    	schema.segments.push(skillObj);
+	 //    }
 
 	    console.log(JSON.stringify(schema,null,2));
 
@@ -311,11 +449,39 @@ $(function (){
             contentType: 'application/json',
             data: JSON.stringify(schema),
             dataType: 'text',
-            success: function(data) {window.open(data)},
-            error: function (xhr, ajaxOptions, thrownError) {console.log(xhr.responseText, thrownError)}
+            success: function(data) {window.open(data);},
+            error: function (xhr, ajaxOptions, thrownError) {console.log(xhr.responseText, thrownError);}
         });
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
