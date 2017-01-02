a55757c5fceddb5e8d08ea84bfa61a5f8778afae
JeremyEphron <jeremyephron@gmail.com>
2017-01-02 12:52:46 -0800

    d

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+connection.query('SELECT 1 + 3 AS solution', function(err, rows, fields) {
   if (err) throw err;
 
   console.log('The solution is: ', rows[0].solution);
