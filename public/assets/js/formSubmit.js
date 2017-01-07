b52e3b5e5e8bf93f76490aded8f42f2cd95d43d0
JeremyEphron <jeremyephron@gmail.com>
2017-01-06 19:17:56 -0800

    forgot contact

--- public/assets/js/formSubmit.js
+++ public/assets/js/formSubmit.js
@@ -52,7 +52,7 @@ $(function (){
             let candidateText = '';
 
             if (degree && major) {
-                candidateText = degree + " in " major;
+                candidateText = degree + " in " + major;
                 universityObj.lines.push({content: candidateText});
             }
 
