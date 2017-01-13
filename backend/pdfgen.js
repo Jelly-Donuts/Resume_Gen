df3b97480c3b060927a89aab0424aa92cf1dab96
JeremyEphron <jeremyephron@gmail.com>
2017-01-13 11:31:03 -0800

    d

--- backend/pdfgen.js
+++ backend/pdfgen.js
@@ -328,7 +328,7 @@ const sizeFits = function(doc, schema, size) {
 
 	const k = 0;
 	const c = 0;
-	const f = 0.98;
+	const f = 0.7;
 	console.log(docHeight, size * ((lines * f) + (segments * k) + (items * c)));
 	return docHeight >= size * ((lines * f) + (segments * k) + (items * c));
 }
