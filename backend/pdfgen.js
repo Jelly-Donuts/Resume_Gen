6af17263715d78e81635fe187e02c0262e10015f
JeremyEphron <jeremyephron@gmail.com>
2017-01-15 14:20:13 -0800

    resize fix2

--- backend/pdfgen.js
+++ backend/pdfgen.js
@@ -319,7 +319,7 @@ const sizeFits = function(doc, schema, size) {
 
 	const k = 1.14;
 	const c = 0.3;
-	const f = 1.4;
+	const f = 1.3;
 	console.log(docHeight, size * ((lines * f) + (segments * k) + (items * c)));
 	console.log(size, lines, segments, items);
 	return docHeight >= size * ((lines * f) + (segments * k) + (items * c));
