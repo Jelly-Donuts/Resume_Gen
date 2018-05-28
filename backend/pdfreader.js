b0a0c3aca26f862aea8c1e8c3e222525c34b1eda
Daniel Andrews <danielandrewsre@gmail.com>
2018-05-27 18:57:10 -0700

    cleaning up tmp folder

--- backend/pdfreader.js
+++ backend/pdfreader.js
@@ -13,12 +13,12 @@ module.exports = {
 			new PdfReader().parseBuffer(pdfBuffer, function(err, item) {
 				if (item.text && item.text.length >= 6) {
 					if (item.text.substring(0, 6) == "start:" && item.text.substr(-4) == ":end") {
-						console.log(item.text);
-						console.log("sending: " + item.text.slice(6, -4))
 						res.send(JSON.parse(item.text.slice(6, -4)));
 					}
 				}
 			});
 		});
+
+		cmd.run('rm -rf ./tmp/*');
 	}
 }
