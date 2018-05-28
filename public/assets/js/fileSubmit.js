fecb631e0bc9fed7ba8dc495d65b58563e26b88e
Daniel Andrews <danielandrewsre@gmail.com>
2018-05-27 19:07:57 -0700

    working for contact info

--- public/assets/js/fileSubmit.js
+++ public/assets/js/fileSubmit.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {    
     $('#file').change(function (file) {
-
-        //Make sure it's a PDF
-
-        //Call API
         var fd = new FormData();
         fd.append('file', file.target.files[0]);
 
@@ -14,8 +10,8 @@ $(document).ready(function () {
             processData: false,
             data: fd,
             success: function(data) {
-                console.log("DATA:");
                 console.log(data);
+                fillForm(data);
             },
             error: function(xhr, ajaxOptions, thrownError) {
                 console.log(xhr);
@@ -26,3 +22,17 @@ $(document).ready(function () {
         });
     });
 });
+
+function fillForm(data) {
+    $('#contact').find('.firstname').val(data.contact.name.split(' ')[0]);
+    $('#contact').find('.lastname').val(data.contact.name.split(' ')[1]);
+
+    $('#contact').find('.address').val(data.contact.address.split(', ')[0]);
+    $('#contact').find('.city').val(data.contact.address.split(', ')[1]);
+    $('#contact').find('.state').val(data.contact.address.split(', ')[2].split(' ')[0]);
+    $('#contact').find('.zipcode').val(data.contact.address.split(',')[2].split(' ')[2]);
+
+    $('#contact').find('.email').val(data.contact.reach[0]);
+    $('#contact').find('.phone').val(data.contact.reach[1]);
+    $('#contact').find('.website').val(data.contact.reach[2]);
+}
